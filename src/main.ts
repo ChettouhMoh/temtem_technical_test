a32dc5247defa8d3bdbc3eaf9dc0532bc9959151
ChettouhMoh <chettouhmoh80@gmail.com>
2025-08-17 10:34:22 +0100

    add basic configs for swagger security

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,40 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import * as basicAuth from 'express-basic-auth';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Protect Swagger with a password
+  app.use(
+    ['/api/docs', '/api/docs-json'], // Protect both the UI and the JSON endpoint
+    basicAuth({
+      users: { [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD },
+      challenge: true, // Enables the browser prompt
+    }),
+  );
   // Swagger configuration
   const config = new DocumentBuilder()
-    .setTitle('My API')
+    .setTitle('temtem technical test')
     .setDescription('API documentation for temtem')
     .setVersion('1.0')
-    .addBearerAuth() // optional: if you use JWT
+    .addTag('temtem')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document); // docs available at http://localhost:3000/api/docs
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
